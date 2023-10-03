b4c5c279b3d8ec782d453fdf7ab9cfea989ecd70
MHassan196 <hssnkhatri2@gmail.com>
2023-10-03 20:18:35 +0500

    Update Spinner

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./components/MovieCard";
+import spinnerIcon from './spinner.svg'
 
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=33212486"
 
 function App() {
   const [searchItem, setSearchItem] = useState("");
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(()=>{
     searchMovie("Batman")
   },[])
 
   const searchMovie = async (title) =>{
+    setLoading(true);
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json();
     setMovies(data.Search)
+    setLoading(false);
   }
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      searchMovie(searchItem);
+    }
+  };
 
   return (
     <div className="app">
@@ -30,6 +39,7 @@ function App() {
           className="search-input"
           value={searchItem}
           onChange={(e) => setSearchItem(e.target.value)}
+          onKeyUp={handleSearch}
         />
         <img
           src={SearchIcon}
@@ -41,21 +51,24 @@ function App() {
       </div>
 
       <div >
-        {
+      {loading ? (
+          <div className="loading">
+            <img src={spinnerIcon} alt="spinner" className="spinner" />
+            {/* <h3>Loading</h3> */}
+          </div>
+        ) : (
           movies?.length > 0 ? (
             <div className="container">
-              {
-                movies.map((movie)=>(
-                  <MovieCard movie={movie} key={movie.imdbID} />
-                ))
-              }
+              {movies.map((movie)=>(
+                <MovieCard movie={movie} key={movie.imdbID} />
+              ))}
             </div>
           ) : (
             <div className="empty">
               <h2>No Movies Found</h2>
             </div>
           )
-        }
+        )}
       </div>
     </div>
   );

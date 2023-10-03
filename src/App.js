c15ab82be19c040c91fb94e94f31bf2483952239
MHassan196 <hssnkhatri2@gmail.com>
2023-10-03 17:37:11 +0500

    Movie app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./components/MovieCard";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=33212486"
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=33212486"
 
 function App() {
   const [searchItem, setSearchItem] = useState("");
